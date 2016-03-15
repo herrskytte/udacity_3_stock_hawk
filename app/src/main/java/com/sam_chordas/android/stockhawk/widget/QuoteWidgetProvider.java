702cf148c92505bff5aec6a6cedeadc7b39b292f
fsk <fsk@computas.com>
2016-03-15 12:35:37 +0100

    Finished widget

--- app/src/main/java/com/sam_chordas/android/stockhawk/widget/QuoteWidgetProvider.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/QuoteWidgetProvider.java
@@ -1,15 +1,55 @@
 package com.sam_chordas.android.stockhawk.widget;
 
-import android.content.BroadcastReceiver;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v4.app.TaskStackBuilder;
+import android.util.Log;
+import android.widget.RemoteViews;
 
-/**
- * Created by fsk on 11.03.2016.
- */
-public class QuoteWidgetProvider extends BroadcastReceiver {
+import com.sam_chordas.android.stockhawk.R;
+import com.sam_chordas.android.stockhawk.service.StockTaskService;
+import com.sam_chordas.android.stockhawk.ui.MyStocksActivity;
+import com.sam_chordas.android.stockhawk.ui.StockDetailActivity;
+
+public class QuoteWidgetProvider extends AppWidgetProvider {
+
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        Log.e("QuoteWidgetProvider", "onUpdate");
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        for (int appWidgetId : appWidgetIds) {
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_collection);
+
+            Intent intent = new Intent(context, MyStocksActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            views.setOnClickPendingIntent(R.id.widget, pendingIntent);
+
+            // Set up the collection
+            views.setRemoteAdapter(R.id.widget_list, new Intent(context, QuoteWidgetRemoteViewsService.class));
+            Intent clickIntentTemplate = new Intent(context, StockDetailActivity.class);
+            PendingIntent clickPendingIntentTemplate = TaskStackBuilder.create(context)
+                    .addNextIntentWithParentStack(clickIntentTemplate)
+                    .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+            views.setPendingIntentTemplate(R.id.widget_list, clickPendingIntentTemplate);
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+
     @Override
-    public void onReceive(Context context, Intent intent) {
-
+    public void onReceive(@NonNull Context context, @NonNull Intent intent) {
+        super.onReceive(context, intent);
+        if (StockTaskService.ACTION_DATA_UPDATED.equals(intent.getAction())) {
+            Log.e("QuoteWidgetProvider", "onReceive ACTION_DATA_UPDATED");
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            int[] appWidgetIds = appWidgetManager.getAppWidgetIds(
+                    new ComponentName(context, getClass()));
+            appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widget_list);
+        }
     }
 }
--- app/src/main/java/com/sam_chordas/android/stockhawk/widget/QuoteWidgetRemoteViewsService.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/QuoteWidgetRemoteViewsService.java
@@ -1,17 +1,120 @@
 package com.sam_chordas.android.stockhawk.widget;
 
-import android.app.Service;
 import android.content.Intent;
-import android.os.IBinder;
-import android.support.annotation.Nullable;
+import android.database.Cursor;
+import android.os.Binder;
+import android.util.Log;
+import android.widget.AdapterView;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
 
-/**
- * Created by fsk on 11.03.2016.
- */
-public class QuoteWidgetRemoteViewsService extends Service {
-    @Nullable
+import com.sam_chordas.android.stockhawk.R;
+import com.sam_chordas.android.stockhawk.data.QuoteColumns;
+import com.sam_chordas.android.stockhawk.data.QuoteProvider;
+import com.sam_chordas.android.stockhawk.rest.Utils;
+import com.sam_chordas.android.stockhawk.ui.StockDetailActivity;
+
+public class QuoteWidgetRemoteViewsService extends RemoteViewsService {
+
     @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new RemoteViewsFactory() {
+            private Cursor cursor = null;
+
+            @Override
+            public void onCreate() {
+                // Nothing to do
+                Log.e("QuoteRemoteViewsService", "onCreate");
+            }
+
+            @Override
+            public void onDataSetChanged() {
+                Log.e("QuoteRemoteViewsService", "onDataSetChanged");
+                if (cursor != null) {
+                    cursor.close();
+                }
+                // This method is called by the app hosting the widget (e.g., the launcher)
+                // However, our ContentProvider is not exported so it doesn't have access to the
+                // data. Therefore we need to clear (and finally restore) the calling identity so
+                // that calls use our process and permission
+                final long identityToken = Binder.clearCallingIdentity();
+
+                cursor = getContentResolver().query(
+                        QuoteProvider.Quotes.CONTENT_URI,
+                        new String[]{ QuoteColumns._ID,
+                                      QuoteColumns.HASDATA,
+                                      QuoteColumns.SYMBOL,
+                                      QuoteColumns.BIDPRICE,
+                                      QuoteColumns.PERCENT_CHANGE,
+                                      QuoteColumns.CHANGE,
+                                      QuoteColumns.ISUP},
+                        QuoteColumns.ISCURRENT + "=? AND " + QuoteColumns.HASDATA + "=?",
+                        new String[]{"1", "1"},
+                        null);
+
+                Binder.restoreCallingIdentity(identityToken);
+            }
+
+            @Override
+            public void onDestroy() {
+                Log.e("QuoteRemoteViewsService", "onDestroy");
+
+                if (cursor != null) {
+                    cursor.close();
+                    cursor = null;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return cursor == null ? 0 : cursor.getCount();
+            }
+
+            @Override
+            public RemoteViews getViewAt(int position) {
+                Log.e("QuoteRemoteViewsService", "getViewAt " + position);
+                if (position == AdapterView.INVALID_POSITION ||
+                        cursor == null || !cursor.moveToPosition(position)) {
+                    return null;
+                }
+
+                RemoteViews views = new RemoteViews(getPackageName(),
+                        R.layout.widget_collection_item);
+
+                views.setTextViewText(R.id.stock_symbol, cursor.getString(cursor.getColumnIndex(QuoteColumns.SYMBOL)));
+                if (Utils.showPercent){
+                    views.setTextViewText(R.id.change, cursor.getString(cursor.getColumnIndex(QuoteColumns.PERCENT_CHANGE)));
+                } else{
+                    views.setTextViewText(R.id.change, cursor.getString(cursor.getColumnIndex(QuoteColumns.CHANGE)));
+                }
+
+                final Intent fillInIntent = new Intent();
+                fillInIntent.putExtra(StockDetailActivity.STOCK_ID_EXTRA, cursor.getLong(cursor.getColumnIndex(QuoteColumns._ID)));
+                views.setOnClickFillInIntent(R.id.widget_list_item, fillInIntent);
+                return views;
+            }
+
+            @Override
+            public RemoteViews getLoadingView() {
+                return new RemoteViews(getPackageName(), R.layout.widget_collection_item);
+            }
+
+            @Override
+            public int getViewTypeCount() {
+                return 1;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                if (cursor.moveToPosition(position))
+                    return cursor.getLong(cursor.getColumnIndex(QuoteColumns._ID));
+                return position;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return true;
+            }
+        };
     }
 }
